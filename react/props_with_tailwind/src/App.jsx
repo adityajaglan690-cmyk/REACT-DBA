ff67cbef2c0923791982ad4f3727023e32a21bb6
Aditya jaglan <adityajaglan690@gmail.com>
2026-08-06 20:02:31 +0530

    6/8/26

---
+++ react/props_with_tailwind/src/App.jsx
@@ -0,0 +1,17 @@
+import 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+import Cart from './component/cart'
+
+function App() {
+
+  return (
+  <>
+    <Cart />
+  </>
+  )
+}
+
+export default App
---
+++ react/props_with_tailwind/src/component/cart.jsx
@@ -0,0 +1,11 @@
+import  "react";
+
+function Cart() {
+  return (
+    <div>
+      <h1>main cart</h1>
+    </div>
+  );
+}
+
+export default Cart;
